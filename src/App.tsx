229f85d11d27f077968b02486d30666ff3d63249
Dharun Kumar  S H <freebirdprod.official@gmail.com>
2025-10-03 09:56:21 +0530

    g

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Router, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -7,22 +7,16 @@ import Landing from "@/pages/Landing";
 import PhotoStudio from "@/pages/PhotoStudio";
 import NotFound from "@/pages/not-found";
 
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Landing} />
-      <Route path="/photo-studio" component={PhotoStudio} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
-
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+        <Router>
+          <Route path="/" component={Landing} />
+          <Route path="/photo-studio" component={PhotoStudio} />
+          <Route component={NotFound} />
+        </Router>
       </TooltipProvider>
     </QueryClientProvider>
   );
